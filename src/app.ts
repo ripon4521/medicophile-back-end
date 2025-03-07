1488d51942af98cd7d0737cb435b82ac79fde694
mosiur411 <mosiurislamwebdesign@gmail.com>
2025-03-07 12:12:10 +0600

    new code add

--- src/app.ts
+++ src/app.ts
@@ -10,7 +10,7 @@ const app = express()
 //parsers
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: ['http://localhost:5173'] }));
+app.use(cors({ origin: ['http://localhost:5173','https://epiccoder.hac.soukhin.shop'] }));
 
 
 // middleware
