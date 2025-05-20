c23f9acd320b0dbd3ac45462047e7673c5d68f48
ripon4521 <riponakondo4521@gmail.com>
2025-05-20 23:54:43 +0600

    updated

--- src/module/qecodeGenerate/qrCodeGenerate.service.ts
+++ src/module/qecodeGenerate/qrCodeGenerate.service.ts
@@ -7,17 +7,17 @@ import { StatusCodes } from "http-status-codes";
 import QueryBuilder from "../../builder/querybuilder";
 
 const generateQrCodeForStudent = async (studentId: string) => {
-  const student = await PurchaseModel.findOne({ studentId });
-  if (!student) {
-    throw new AppError(StatusCodes.NOT_FOUND, "Student not found in purchase");
-  }
+  // const student = await PurchaseModel.findOne({ studentId });
+  // if (!student) {
+  //   throw new AppError(StatusCodes.NOT_FOUND, "Student not found in purchase");
+  // }
 
   const batchStudent = await BatchStudentModel.findOne({ studentId });
   if (!batchStudent) {
     throw new AppError(StatusCodes.NOT_FOUND, "Student not admitted in batch");
   }
 
-  const qrData = student._id.toString();
+  const qrData = studentId.toString();
   const qrImage = await QRCode.toDataURL(qrData);
 
   const result = await qrCodeModel.create({ qrCode: qrImage });
