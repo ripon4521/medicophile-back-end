aa1b7310e70a2c1d1ab79d82ff0bf8f6f100d4fe
ripon4521 <riponakondo4521@gmail.com>
2025-05-22 13:08:28 +0600

    updated

--- src/module/course/course.router.ts
+++ src/module/course/course.router.ts
@@ -2,22 +2,24 @@ import express from "express";
 import validateRequest from "../../middlewares/validateRequest";
 import { courseValidation } from "./course.validation";
 import { courseController } from "./course.controller";
-import { auth } from "../../middlewares/auth";
+import { auth, authUser } from "../../middlewares/auth";
 
 const courseRouter = express.Router();
 courseRouter.post(
   "/create-course",
+  authUser("admin", "teacher","superAdmin"),
   validateRequest(courseValidation.createCourseSchema),
   courseController.createCourse,
 );
-courseRouter.get("/", courseController.getAllCourses);
-courseRouter.get("/my-course", courseController.getMyCourse);
+courseRouter.get("/", authUser("admin", "teacher", "superAdmin"), courseController.getAllCourses);
+courseRouter.get("/my-course",authUser("student"), courseController.getMyCourse);
 courseRouter.get("/:slug", auth.authUser(), courseController.getSingleCourse);
 courseRouter.patch(
   "/:slug",
+  authUser("admin", "teacher", "superAdmin"),
   validateRequest(courseValidation.updateCourseSchema),
   courseController.updateCourse,
 );
-courseRouter.delete("/:slug", courseController.deleteCourse);
+courseRouter.delete("/:slug", authUser("admin", "superAdmin", "teacher"), courseController.deleteCourse);
 
 export default courseRouter;
