04268199433baea189ce2705d84c70e9fc4f7a10
ripon4521 <riponakondo4521@gmail.com>
2025-05-25 22:28:41 +0600

    updated

--- src/module/paymentDetails/paymentDetails.controller.ts
+++ src/module/paymentDetails/paymentDetails.controller.ts
@@ -4,7 +4,8 @@ import sendResponse from "../../utils/sendResponse";
 import { pamentDetailsService } from "./paymentDetails.service";
 
 const getAllPamentyDetails = catchAsync(async (req, res) => {
-  const result = await pamentDetailsService.getAllPaymentDetails();
+  const query = req.query;
+  const result = await pamentDetailsService.getAllPaymentDetails(query);
   sendResponse(res, {
     statusCode: StatusCodes.OK,
     message: "Payment Details get successfully",
--- src/module/paymentDetails/paymentDetails.service.ts
+++ src/module/paymentDetails/paymentDetails.service.ts
@@ -1,9 +1,24 @@
+import QueryBuilder from "../../builder/querybuilder";
 import PaymentDetailsModel from "./paymentDetails.model";
 
-const getAllPaymentDetails = async () => {
-  const result = await PaymentDetailsModel.find({ isDeleted: false });
+// const getAllPaymentDetails = async () => {
+//   const result = await PaymentDetailsModel.find({ isDeleted: false });
+//   return result;
+// };
+
+const getAllPaymentDetails = async (query: Record<string, unknown>) => {
+  const courseQuery = new QueryBuilder(PaymentDetailsModel, query)
+    .search(["method", "accountNumber"])
+    .filter()
+    .sort()
+    .paginate()
+    .fields()
+
+  const result = await courseQuery.exec();
   return result;
 };
+
+
 
 export const pamentDetailsService = {
   getAllPaymentDetails,
