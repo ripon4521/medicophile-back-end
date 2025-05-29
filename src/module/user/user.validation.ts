ad9f353e1de230ed487990e5c96d2be2fbd1b14e
ripon4521 <riponakondo4521@gmail.com>
2025-05-29 21:24:48 +0600

    updated

--- src/module/user/user.validation.ts
+++ src/module/user/user.validation.ts
@@ -29,8 +29,8 @@ export const changePasswordValidation = z.object({
   body: z.object({
     phone: z.string({ required_error: "phone number is required" }),
     oldPassword: z.string({ required_error: "old password is required" }),
-    newPassword: z.string({ required_error: "new password is required" }),
-    confrimPassword: z.string({
+    newPassord: z.string({ required_error: "new password is required" }),
+    confrimPassord: z.string({
       required_error: "confrim password is required",
     }),
   }), 
